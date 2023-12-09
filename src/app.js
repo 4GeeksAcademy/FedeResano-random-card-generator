556879f75c8f9f7e0b105b755b7604d1284b0a4a
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-12-09 15:52:58 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  pickACard();
+  document.querySelector("#button").addEventListener("click", () => {
+    pickACard();
+  });
 };
+
+function pickACard() {
+  let suits = ["diamonds", "clubs", "hearts", "clover"];
+  let cardValues = ["A", 2, ...12, "J", "Q", "K"];
+
+  let a = Math.floor(Math.random() * suits.length - 1);
+  let b = Math.floor(Math.random() * cardValues.length - 1);
+
+  let card = {
+    suit: suits[a],
+    cardValue: cardValues[b],
+  };
+
+  
+
+
+
+
+}
