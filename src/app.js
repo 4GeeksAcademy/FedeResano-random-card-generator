0076c9e835b2dabfa4c5bf666ce7e2c88f561845
Federico Resano <federicoresano1@gmail.com>
2023-12-10 01:30:16 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,30 +1,57 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  //write your code here
+window.onload = function () {
   pickACard();
   document.querySelector("#button").addEventListener("click", () => {
-    pickACard();
+    createCard(pickACard());
   });
 };
 
+// Icons : ♦ ♥ ♠ ♣
+
+const card = {
+  suits: ["diamonds", "clubs", "hearts", "clovers"],
+  cardValues: ["A", 2, ...12, "J", "Q", "K"]
+};
+
+
 function pickACard() {
-  let suits = ["diamonds", "clubs", "hearts", "clover"];
-  let cardValues = ["A", 2, ...12, "J", "Q", "K"];
 
-  let a = Math.floor(Math.random() * suits.length - 1);
-  let b = Math.floor(Math.random() * cardValues.length - 1);
+  let a = Math.floor(Math.random() * card.suits.length + 1);
+  let b = Math.floor(Math.random() * card.cardValues.length + 1);
 
-  let card = {
-    suit: suits[a],
-    cardValue: cardValues[b],
-  };
+  let selectedCard = card[suits[a]][cardValues[b]];
 
-  
+  return selectedCard;
+}
 
 
+function createCard(cardObject) {
+  let suitStringLeft = "";
+  let suitStringRight = "";
+  let valueString = cardObject[cardValues];
 
 
+  switch (cardObject[suits]) {
+
+    case "diamonds":
+      suitStringLeft = '<i class="fa-solid fa-diamond fa-align-left" style="color: #ff0000;"></i>';
+      suitStringRight = '<i class="fa-solid fa-diamond fa-align-right fa-rotate-90" style="color: #ff0000;"></i>';
+      break;
+    case "hearts":
+      suitStringLeft = '';
+      suitStringRight = '';
+      break;
+    case "clovers":
+      suitStringLeft = '';
+      suitStringRight = '';
+      break;
+    case "clubs":
+      suitStringLeft = '';
+      suitStringRight = '';
+      break;
+  }
+
+  return suitStringLeft + " " + valueString + " " + suitStringRight;
 }
