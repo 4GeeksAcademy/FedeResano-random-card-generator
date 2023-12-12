6468f72731587dd7543262473eb65f935f424f2d
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-12-12 01:30:38 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function () {
+window.onload = function() {
   createCard(pickACard());
   document.querySelector("#button").addEventListener("click", () => {
     createCard(pickACard());
@@ -10,59 +10,30 @@ window.onload = function () {
 
 // Icons : ♦ ♥ ♠ ♣
 
-const card = {
-  suits: ["diamonds", "clubs", "hearts", "clovers"],
-  cardValues: [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ]
+const CARD = {
+  suits: ["♦", "♥", "♠", "♣"],
+  cardValues: ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 };
 
 function pickACard() {
-  let a = Math.floor(Math.random() * card.suits.length);
-  let b = Math.floor(Math.random() * card.cardValues.length);
+  let a = Math.floor(Math.random() * CARD.suits.length);
+  let b = Math.floor(Math.random() * CARD.cardValues.length);
 
   let selectedCard = {
-    suit: card.suits[a],
-    value: card.cardValues[b]
+    suit: CARD.suits[a],
+    value: CARD.cardValues[b]
   };
   return selectedCard;
 }
 
 function createCard(cardObject) {
-  let suitString = "";
+  let suitString = cardObject.suit;
   let valueString = cardObject.value;
-
-  switch (cardObject.suit) {
-    case "diamonds":
-      suitString = "♦";
-      break;
-    case "hearts":
-      suitString = "♥";
-      break;
-    case "clovers":
-      suitString = "♣";
-      break;
-    case "clubs":
-      suitString = "♠";
-      break;
-  }
 
   document.querySelector(".suit").innerHTML = suitString;
   document.querySelector(".rotated").innerHTML = suitString;
 
-  if (cardObject.suit === "clubs" || cardObject.suit === "clovers") {
+  if (cardObject.suit === "♠" || cardObject.suit === "♣") {
     document.querySelector(".suit").classList.add("black");
     document.querySelector(".rotated").classList.add("black");
   } else {
